786524b45eb0447be6f9d1917263184c3d853822
jmcheon <jm@JM-MacBookPro.local>
2023-04-05 22:57:50 +0900

    Feat: Springdoc OpenApi-ui (Swagger3.0) 추가

---
+++ src/main/java/com/jm/portfolio/global/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.jm.portfolio.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String springdocVersion) {
+        Info info = new Info()
+                .title("Portfolio API Document")
+                .version(springdocVersion)
+                .description("API Document for portfolio project testing");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+
+}
