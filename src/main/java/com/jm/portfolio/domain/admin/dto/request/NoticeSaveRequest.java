fb0cd521218ab7cedd3a7c88f035247bd0e75288
jmcheon <jm@JM-MacBookPro.local>
2023-05-09 20:39:01 +0900

    try

--- src/main/java/com/jm/portfolio/domain/admin/dto/request/NoticeSaveRequest.java
+++ src/main/java/com/jm/portfolio/domain/admin/dto/request/NoticeSaveRequest.java
@@ -14,6 +14,7 @@ public class NoticeSaveRequest extends BaseRequest {
     private String title;
     private String content;
     private String img;
+    
 
     public Notice toEntity() {
         return Notice.builder()
